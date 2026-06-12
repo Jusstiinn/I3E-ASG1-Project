b862a300519d5979204d279910c60e94b3481f2c
Justin <S10273684@connect.np.edu.sg>
2026-06-12 23:22:41 +0800

    Added UI and did some bug fixes

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    PlayerScript playerScript;
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+    }
+
+    void Update()
+    {
+        if (playerScript != null)
+        {
+            scoreText.text = "Crystals Shattered: " + playerScript.crystalCount;
+        }
+    }
+}
